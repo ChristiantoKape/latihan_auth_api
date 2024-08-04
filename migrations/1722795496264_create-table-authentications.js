7768a86e485c2ee55aa0cadba0afc7cb38a3a3f4
Christianto Kurniawan <christiantokape@gmail.com>
2024-08-05 01:20:39 +0700

    adding migrations for users and authentications

---
+++ migrations/1722795496264_create-table-authentications.js
@@ -0,0 +1,14 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('authentications', {
+    token: {
+      type: 'TEXT',
+      primaryKey: true,
+    },
+  });
+}
+
+exports.down = (pgm) => {
+  pgm.dropTable('authentications');
+}
